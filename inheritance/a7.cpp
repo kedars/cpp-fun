3be13fdfe55f67101e318abc68b60d6a2ba8140f
Kedar Sovani <kedars@gmail.com>
2020-07-04 12:49:02 +0530

    virtual member functions

---
+++ inheritance/a7.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+/** 
+ * Overriding members without virtual keyword
+ */
+
+/*************** Class Definitions Start ***************/
+class base {
+public:
+	base() {  }
+	~base() { }
+        void test(void) { cout <<  "  base: test\n"; }
+};
+
+/********* New Derive Class ********/
+class derived: public base {
+public:
+	derived() { }
+	~derived() { }
+        void test(void) { cout <<  "  derived: test\n"; }
+};
+
+class basevirtual {
+public:
+        virtual void test(void) { cout <<  "  base: test\n"; }
+};
+
+/********* New Derive Class ********/
+class derivedvirtual: public basevirtual {
+public:
+
+        void test(void) { cout <<  "  derived: test\n"; }
+};
+
+/*************** Class Definitions End ***************/
+	
+int main()
+{
+        derived d;
+        cout << "Calling derived.test() \n";
+        d.test();
+
+        derived e;
+        base *b = &e;
+        cout << "Calling base-ptr.test() \n";
+        b->test();
+
+        /* The basevirtual class defines the test() function as
+         * 'virtual', hence when called with base-ptr c->test(), the derived
+         * class's test() is called.
+         *
+         * as opposed to the behaviour above, where b->test() invokes
+         * the base class' test().
+         */
+        derivedvirtual f;
+        basevirtual *c = &f;
+        cout << "Calling base-ptr-virtual.test()\n";
+        c->test();
+        return 0;
+}
