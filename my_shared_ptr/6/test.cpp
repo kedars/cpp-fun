a4ef6202d37c2948da8b9161be5ba547fc6ddd83
Kedar Sovani <kedars@gmail.com>
2020-08-30 19:52:56 +0530

    Create a simple test suite

--- my_shared_ptr/6/test.cpp
+++ my_shared_ptr/6/test.cpp
@@ -1,19 +1,57 @@
 #include <stdio.h>
+#include <string.h>
 #include "MySharedPtr.h"
 
-#define my_assert(string, condition) \
+#define MAX_REGISTERED_TESTS  10
+static struct test_desc {
+    const char *td_name;
+    void (*td_func)(void);
+} g_registered_tests[MAX_REGISTERED_TESTS];
+static int g_total_tests;
+static bool g_test_success;
+
+/* Standard test framework, can be moved to a separate file */
+#define etest_assert(string, condition) \
     do { \
        if (!(condition)) { \
            printf("[%s:%d] Error: %s\n", __FILE__, __LINE__, string);    \
+           g_test_success = false; \
        } \
     } while(0)
 
-#define my_assert_str(string, expected, actual) \
+#define etest_assert_str(string, expected, actual) \
     do { \
         if (strcmp(expected, actual) != 0) {    \
            printf("[%s:%d] Error: %s Expected:%s: Received:%s:\n", __FILE__, __LINE__, string, expected, actual); \
+           g_test_success = false; \
        } \
     } while(0)
+
+    
+void etest_tests_register(const char *name, void (*func)(void))
+{
+    if (g_total_tests > MAX_REGISTERED_TESTS) {
+        printf("Error: Failed to register tests: limit reached\n");
+        return;
+    }
+    g_registered_tests[g_total_tests].td_name = name;
+    g_registered_tests[g_total_tests].td_func = func;
+    g_total_tests++;
+}
+
+void etest_tests_execute()
+{
+    for (int i = 0; i < g_total_tests; i++) {
+        g_test_success = true;
+        printf("%s: ", g_registered_tests[i].td_name);
+        (*g_registered_tests[i].td_func)();
+        if (g_test_success) {
+            printf("Passed\n");
+        } else {
+            printf("Failed\n");
+        }
+    }
+}
 
 enum state {
     UNINIT = 0,
@@ -42,22 +80,22 @@ public:
 
 static const char *basic_str = "Basic Str";
 static const char *overwritten_str = "Overwritte Str";
-void test_basics()
+void test_basics(void)
 {
     struct test_data basic_data(basic_str);
     {
         MySharedPtr<TestObject> ptr(new TestObject(&basic_data));
         /* Test that pointer deference points to the correct object */
-        my_assert_str("Pointer dereference", ptr->mPtr->mStr, basic_str);
+        etest_assert_str("Pointer dereference", ptr->mPtr->mStr, basic_str);
         /* Test default reference count is 1 */
-        my_assert("Default ref 1", ptr.getCnt() == 1);
-        my_assert("Object Constructed", basic_data.mState == CONSTRUCTED);
+        etest_assert("Default ref 1", ptr.getCnt() == 1);
+        etest_assert("Object Constructed", basic_data.mState == CONSTRUCTED);
     }
     /* Test that the 'ptr' was destructed */
-    my_assert("Object Destructed", basic_data.mState == DESTRUCTED);
+    etest_assert("Object Destructed", basic_data.mState == DESTRUCTED);
 }
 
-void test_assignment()
+void test_assignment(void)
 {
     struct test_data basic_data(basic_str);
     {
@@ -65,20 +103,20 @@ void test_assignment()
         {
             MySharedPtr<TestObject> ptr2(ptr1);
             /* Test that pointer deference points to the correct object */
-            my_assert_str("Assigned pointer dereference", ptr2->mPtr->mStr, basic_str);
+            etest_assert_str("Assigned pointer dereference", ptr2->mPtr->mStr, basic_str);
             /* Test reference count is 2 due to assignment */
-            my_assert("RefCount 2", ptr2.getCnt() == 2);
-            my_assert("Object Constructed", basic_data.mState == CONSTRUCTED);
+            etest_assert("RefCount 2", ptr2.getCnt() == 2);
+            etest_assert("Object Constructed", basic_data.mState == CONSTRUCTED);
         }
         /* Test reference count is 1 because one ptr is destroyed */
-        my_assert("RefCount 1", ptr1.getCnt() == 1);
-        my_assert("Object Constructed", basic_data.mState == CONSTRUCTED);
+        etest_assert("RefCount 1", ptr1.getCnt() == 1);
+        etest_assert("Object Constructed", basic_data.mState == CONSTRUCTED);
     }
     /* Test that the object was destructed as all reference are dropped */
-    my_assert("Object Destructed", basic_data.mState == DESTRUCTED);
+    etest_assert("Object Destructed", basic_data.mState == DESTRUCTED);
 }
 
-void test_assignment_overwrite()
+void test_assignment_overwrite(void)
 {
     struct test_data basic_data(basic_str);
     struct test_data overwritten_data(overwritten_str);
@@ -88,20 +126,22 @@ void test_assignment_overwrite()
             MySharedPtr<TestObject> ptr2(new TestObject(&overwritten_data));
             ptr1 = ptr2;
             /* Test that basic-data is destroyed */
-            my_assert_str("Assigned pointer dereferences to new data", ptr1->mPtr->mStr, overwritten_str);
+            etest_assert_str("Assigned pointer dereferences to new data", ptr1->mPtr->mStr, overwritten_str);
             /* Test reference count is 2 due to assignment */
-            my_assert("RefCount 2", ptr2.getCnt() == 2);
-            my_assert("Old Object Destructed", basic_data.mState == DESTRUCTED);
-            my_assert("New Object Constructed", overwritten_data.mState == CONSTRUCTED);
+            etest_assert("RefCount 2", ptr2.getCnt() == 2);
+            etest_assert("Old Object Destructed", basic_data.mState == DESTRUCTED);
+            etest_assert("New Object Constructed", overwritten_data.mState == CONSTRUCTED);
         }
     }
     /* Test that the object was destructed as all reference are dropped */
-    my_assert("Object Destructed", overwritten_data.mState == DESTRUCTED);
+    etest_assert("Object Destructed", overwritten_data.mState == DESTRUCTED);
 }
+
 
 int main(void)
 {
-    test_basics();
-    test_assignment();
-    test_assignment_overwrite();
+    etest_tests_register("[SharedPtr] Basic", &test_basics);
+    etest_tests_register("[SharedPtr] Assignment", &test_assignment);
+    etest_tests_register("[SharedPtr] Overwrite with Assignment", &test_assignment_overwrite);
+    etest_tests_execute();
 }
